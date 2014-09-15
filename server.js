1f5be9282c1f7acf7ab5775c5b2d5a7f2881d461
damrem <damien.remars@gmail.com>
2014-09-16 00:56:06 +0200

    less crossdomain

--- server.js
+++ server.js
@@ -48,9 +48,9 @@ app.get('/', function (req, res){
 
 app.get('/getallusers', function(req, res, next){
 	//required for crossdomain
-	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization");
-	res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+	//res.header("Access-Control-Allow-Origin", "*");
+	//res.header("Access-Control-Allow-Headers", "X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization");
+	//res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
 	
 
 	db.things.find('', function(err, users){
